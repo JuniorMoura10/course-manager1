d2c768697a69f1d9a5029b24f2ed7c4393b8bb67
Júnior Moura <geraldomartins10@gmail.com>
2022-05-26 20:36:07 -0300

    injeção de dependências com decorator @Injectable

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { Course } from "./course";
+import { CouseService } from "./course-service";
 
 @Component({
      selector: 'app-course-list',
@@ -9,29 +10,12 @@ export class CourseListComponent implements OnInit{
 
     courses: Course[] = [];
 
+    constructor(private courseService: CouseService){
+
+    }
+
     ngOnInit(): void {
-        this.courses = [
-            {
-                id: 1,
-                name: 'Angular: Forms',
-                imageUrl: '/assets/images/forms.png',
-                price: 99.99,
-                code: 'XTS - 8796',
-                duration: 120,
-                rating: 4.5,
-                releaseDate: 'December, 2, 2019'
-            },
-            {
-                id: 2,
-                name: 'Angular: HTTP',
-                imageUrl: '/assets/images/http.png',
-                price: 45.99,
-                code: 'LTS - 1094',
-                duration: 80,
-                rating: 4,
-                releaseDate: 'December, 4, 2019'
-            }
-        ]
+        this.courses = this.courseService.retrieveAll();
     }
     
 }
--- src/app/courses/course.ts
+++ src/app/courses/course.ts
@@ -7,5 +7,6 @@ export class Course {
     duration: number = 0;
     rating: number = 0;
     releaseDate: string = '';
+    description: string = '';
 
 }
